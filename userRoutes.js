e037dffa8c1b4561c172b3a978f8a7e1e4acea93
Teddi Maull <teddimaull@gmail.com>
2019-08-13 16:30:18 -0600

    add a router for user routes

---
+++ userRoutes.js
@@ -0,0 +1,16 @@
+let userRoutes = require('express').Router();
+
+userRoutes.get('/', function(req,res){
+  console.log("params",req.params);
+  console.log("query",req.query);
+  res.send("please enter a name parameter");
+});
+
+//in browser try http://localhost:3000/api/user/teddi?why=because
+userRoutes.get('/:name', function(req,res){
+  console.log("params",req.params);
+  console.log("query",req.query);
+  res.send({paramreceived:req.params, queryreceived: req.query});
+});
+
+module.exports = userRoutes;
