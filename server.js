0288e5e94bd67869b70c8ff5f347d11baadf11b6
Teddi Maull <teddimaull@gmail.com>
2019-08-13 16:30:41 -0600

    update server to utilize the new router routes

--- server.js
+++ server.js
@@ -2,21 +2,14 @@
 
 var express = require('express');
 var app = express();
+var mainRoutes = require('./mainRoutes');
+var userRoutes = require('./userRoutes');
 
 app.use(express.static('public'));
 
-app.get('/api', function(req,res){
-  console.log("query",req.query);
-  res.status(200);
-  res.send(req.query);
-});
+app.use('/api', mainRoutes);
+app.use('/api/user', userRoutes);
 
-
-app.get('/user/:name', function(req,res){
-  console.log("params",req.params);
-  console.log("query",req.query);
-  res.send(req.params);
-});
 
 app.listen('3000',function(){
   console.log("listening on port 3000");
