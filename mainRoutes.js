a58af10aeaa302c2fe8ff07ebea09269f9f0199f
Teddi Maull <teddimaull@gmail.com>
2019-08-13 16:30:10 -0600

    add a router for main routes

---
+++ mainRoutes.js
@@ -0,0 +1,21 @@
+let mainRoutes = require('express').Router();
+
+
+mainRoutes.get('/', function(req,res,next) {
+    console.log("query",req.query);
+    res.status(200);
+    res.send("hello world");
+});
+
+mainRoutes.post('/', function(req,res,next) {
+    let response = "";
+    if (req.body === undefined || Object.keys(req.body).length === 0) {
+      response = "I didn't see anything anything in the body";
+    } else {
+      response = "message received";
+
+    }
+    res.send(response);
+});
+
+module.exports = mainRoutes;
